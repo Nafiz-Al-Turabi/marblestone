ffddd2ff10af69d449a4bc5a3d85b6399f9ec8cd
unknown <nafizalturabi18cse@gmail.com>
2024-08-22 17:03:31 +0600

    small changes

--- src/PropertyComponents/PropertyBanner/PropertyBanner.jsx
+++ src/PropertyComponents/PropertyBanner/PropertyBanner.jsx
@@ -4,7 +4,7 @@ import { AiFillHome } from 'react-icons/ai';
 const PropertyBanner = () => {
     return (
         <div className='lg:m-6 '>
-            <div className=' bg-black px-16 py-20 md:px-32 lg:p-40  lg:rounded-3xl relative mb-44 '>
+            <div className=' bg-black px-16 py-20 md:px-32 lg:p-40  lg:rounded-3xl xl:relative mb-44 '>
                 <div className='text-center'>
                     <div className="flex justify-center ">
                         <button className="text-xs flex items-center text-white bg-gray-900 opacity-80 gap-1 justify-center p-2 rounded-full mb-4  ">
@@ -17,27 +17,27 @@ const PropertyBanner = () => {
                         <p className='secondary-text'>Lorem ipsum dolor sit amet consectetur. Sit ut gravida aenean potenti. Metus in eu vel morbi dui nunc tellus. Non a massa maecenas massa.</p>
                     </div>
                 </div>
-                <form className='bg-white w-[940px] mx-auto p-6 rounded-3xl absolute left-1/4 mt-10'>
-                    <div className='flex gap-1 mb-4'>
-                        <div className='w-[40%]'>
+                <form className='bg-white w-full  xl:w-[940px] mx-auto p-6 rounded-3xl xl:absolute left-1/4 mt-10'>
+                    <div className='xl:flex gap-1 mb-4'>
+                        <div className='xl:w-[40%]'>
                             <label className='block mb-4 font-semibold text-gray-600'>
                                 Search
                             </label>
                             <input type="text" className='w-full secondary-bg secodary-text p-[11px] focus:outline-none rounded-l-full' placeholder='Search for properties' />
                         </div>
-                        <div className='w-[20%]'>
+                        <div className='xl:w-[20%]'>
                             <label className='block mb-4 font-semibold text-gray-600'>
                                 Min Rent
                             </label>
                             <input type="text" className='w-full secondary-bg secodary-text p-[11px] focus:outline-none' />
                         </div>
-                        <div className='w-[20%]'>
+                        <div className='xl:w-[20%]'>
                             <label className='block mb-4 font-semibold text-gray-600'>
                                 Max Rent
                             </label>
                             <input type="text" className='w-full secondary-bg secodary-text p-[11px] focus:outline-none' />
                         </div>
-                        <div className='w-[10%]'>
+                        <div className='xl:w-[10%]'>
                             <label className='block mb-4 font-semibold text-gray-600'>
                                 Bed
                             </label>
@@ -49,7 +49,7 @@ const PropertyBanner = () => {
                                 <option value="4">4+</option>
                             </select>
                         </div>
-                        <div className='w-[10%]'>
+                        <div className='xl:w-[10%]'>
                             <label className='block mb-4 font-semibold text-gray-600'>
                                 Bath
                             </label>
@@ -62,8 +62,8 @@ const PropertyBanner = () => {
                             </select>
                         </div>
                     </div>
-                    <div className='flex gap-1'>
-                        <div className='w-[33.33%]'>
+                    <div className='xl:flex gap-1'>
+                        <div className='xl:w-[33.33%]'>
                             <label className='block mb-4 font-semibold text-gray-600'>
                                 Property Type
                             </label>
@@ -75,7 +75,7 @@ const PropertyBanner = () => {
                                 <option value="townhouse">Townhouse</option>
                             </select>
                         </div>
-                        <div className='w-[33.33%]'>
+                        <div className='xl:w-[33.33%]'>
                             <label className='block mb-4 font-semibold text-gray-600'>
                                 Pets Policy
                             </label>
@@ -86,7 +86,7 @@ const PropertyBanner = () => {
                                 <option value="negotiable">Negotiable</option>
                             </select>
                         </div>
-                        <div className='w-[33.33%]'>
+                        <div className='xl:w-[33.33%]'>
                             <label className='block mb-4 font-semibold text-gray-600'>
                                 Sort
                             </label>
