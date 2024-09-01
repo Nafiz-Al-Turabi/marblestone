8fb9bff4416785dc1b41fb4448d308ba06d9e8f9
unknown <nafizalturabi18cse@gmail.com>
2024-09-01 16:29:33 +0600

    Dynamic agent data added in about page

--- src/AboutComponents/OurTeam/OurTeam.jsx
+++ src/AboutComponents/OurTeam/OurTeam.jsx
@@ -3,12 +3,27 @@ import { FaArrowRight, FaPhoneAlt } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa6";
 
 import { MdMail } from "react-icons/md";
-import agent1 from './../../assets/AboutImages/agent1.png'
-import agent2 from './../../assets/AboutImages/agent2.png'
-import agent3 from './../../assets/AboutImages/agent3.png'
+import { useEffect, useState } from "react";
+import axiosInstance from "../../Axios/AxiosInstance";
 
 
 const OurTeam = () => {
+    const [agents, setAgents] = useState([]);
+
+    useEffect(() => {
+        fetchAgent();
+    }, []);
+
+    const fetchAgent = async () => {
+        try {
+            const response = await axiosInstance.get('/agents');
+            if (response) {
+                setAgents(response.data)
+            }
+        } catch (error) {
+
+        }
+    }
     return (
         <div className='pt-20 lg:pt-40 lg:px-4 mb-20'>
             <div className='lg:max-w-[1200px] mx-auto  px-6 lg:px-0'>
@@ -25,115 +40,28 @@ const OurTeam = () => {
                     </div>
                     {/* Agents */}
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                        <div className="p-16 md:p-20 lg:p-16 xl:p-24 shadow rounded-3xl bg-white relative">
-                            <div className="flex justify-center">
-                                <img src={agent1} alt="" className="rounded-full" />
-                            </div>
-                            <h1 className="flex justify-center text-base md:text-2xl my-3 font-semibold">John Carter</h1>
-                            <p className="flex justify-center secondary-text mb-4">Inmobiliary Agent</p>
-                            <div className="flex justify-center gap-5">
-                                <button>
-                                    <MdMail className="h-10 w-10 bg-slate-100 rounded-full text-gray-900 p-2" />
-                                </button>
-                                <button>
-                                    <FaPhoneAlt className="h-10 w-10 bg-slate-100 rounded-full text-gray-900 p-2" />
-                                </button>
-                            </div>
-                            <button className="absolute top-5 right-5">
-                                <FaPlus className="h-10 w-10 bg-gray-900 rounded-full text-white p-2" />
-                            </button>
-                        </div>
-                        <div className="p-16 md:p-20 lg:p-16 xl:p-24 shadow rounded-3xl bg-white relative">
-                            <div className="flex justify-center">
-                                <img src={agent3} alt="" className="rounded-full" />
-                            </div>
-                            <h1 className="flex justify-center text-base md:text-2xl my-3 font-semibold">Sophie Moore</h1>
-                            <p className="flex justify-center secondary-text mb-4">Inmobiliary Agent</p>
-                            <div className="flex justify-center gap-5">
-                                <button>
-                                    <MdMail className="h-10 w-10 bg-slate-100 rounded-full text-gray-900 p-2" />
-                                </button>
-                                <button>
-                                    <FaPhoneAlt className="h-10 w-10 bg-slate-100 rounded-full text-gray-900 p-2" />
-                                </button>
-                            </div>
-                            <button className="absolute top-5 right-5">
-                                <FaPlus className="h-10 w-10 bg-gray-900 rounded-full text-white p-2" />
-                            </button>
-                        </div>
-                        <div className="p-16 md:p-20 lg:p-16 xl:p-24 shadow rounded-3xl bg-white relative">
-                            <div className="flex justify-center">
-                                <img src={agent2} alt="" className="rounded-full" />
-                            </div>
-                            <h1 className="flex justify-center text-base md:text-2xl my-3 font-semibold">Matt Cannon</h1>
-                            <p className="flex justify-center secondary-text mb-4">Inmobiliary Agent</p>
-                            <div className="flex justify-center gap-5">
-                                <button>
-                                    <MdMail className="h-10 w-10 bg-slate-100 rounded-full text-gray-900 p-2" />
-                                </button>
-                                <button>
-                                    <FaPhoneAlt className="h-10 w-10 bg-slate-100 rounded-full text-gray-900 p-2" />
-                                </button>
-                            </div>
-                            <button className="absolute top-5 right-5">
-                                <FaPlus className="h-10 w-10 bg-gray-900 rounded-full text-white p-2" />
-                            </button>
-                        </div>
-
-                        <div className="p-16 md:p-20 lg:p-16 xl:p-24 shadow rounded-3xl bg-white relative">
-                            <div className="flex justify-center">
-                                <img src={agent3} alt="" className="rounded-full" />
-                            </div>
-                            <h1 className="flex justify-center text-base md:text-2xl my-3 font-semibold">Sophie Moore</h1>
-                            <p className="flex justify-center secondary-text mb-4">Inmobiliary Agent</p>
-                            <div className="flex justify-center gap-5">
-                                <button>
-                                    <MdMail className="h-10 w-10 bg-slate-100 rounded-full text-gray-900 p-2" />
-                                </button>
-                                <button>
-                                    <FaPhoneAlt className="h-10 w-10 bg-slate-100 rounded-full text-gray-900 p-2" />
-                                </button>
-                            </div>
-                            <button className="absolute top-5 right-5">
-                                <FaPlus className="h-10 w-10 bg-gray-900 rounded-full text-white p-2" />
-                            </button>
-                        </div>
-                        <div className="p-16 md:p-20 lg:p-16 xl:p-24 shadow rounded-3xl bg-white relative">
-                            <div className="flex justify-center">
-                                <img src={agent2} alt="" className="rounded-full" />
-                            </div>
-                            <h1 className="flex justify-center text-base md:text-2xl my-3 font-semibold">Matt Cannon</h1>
-                            <p className="flex justify-center secondary-text mb-4">Inmobiliary Agent</p>
-                            <div className="flex justify-center gap-5">
-                                <button>
-                                    <MdMail className="h-10 w-10 bg-slate-100 rounded-full text-gray-900 p-2" />
-                                </button>
-                                <button>
-                                    <FaPhoneAlt className="h-10 w-10 bg-slate-100 rounded-full text-gray-900 p-2" />
-                                </button>
-                            </div>
-                            <button className="absolute top-5 right-5">
-                                <FaPlus className="h-10 w-10 bg-gray-900 rounded-full text-white p-2" />
-                            </button>
-                        </div>
-                        <div className="p-16 md:p-20 lg:p-16 xl:p-24 shadow rounded-3xl bg-white relative">
-                            <div className="flex justify-center">
-                                <img src={agent1} alt="" className="rounded-full" />
-                            </div>
-                            <h1 className="flex justify-center text-base md:text-2xl my-3 font-semibold">John Carter</h1>
-                            <p className="flex justify-center secondary-text mb-4">Inmobiliary Agent</p>
-                            <div className="flex justify-center gap-5">
-                                <button>
-                                    <MdMail className="h-10 w-10 bg-slate-100 rounded-full text-gray-900 p-2" />
-                                </button>
-                                <button>
-                                    <FaPhoneAlt className="h-10 w-10 bg-slate-100 rounded-full text-gray-900 p-2" />
-                                </button>
-                            </div>
-                            <button className="absolute top-5 right-5">
-                                <FaPlus className="h-10 w-10 bg-gray-900 rounded-full text-white p-2" />
-                            </button>
-                        </div>
+                        {
+                            agents.map(agent => (
+                                <div key={agent._id} className="p-16 md:p-20 lg:p-16 xl:p-24 shadow rounded-3xl bg-white relative">
+                                    <div className="flex justify-center">
+                                        <img src={agent.image} alt="" className="rounded-full" />
+                                    </div>
+                                    <h1 className="flex justify-center text-base md:text-2xl my-3 font-semibold">{agent.name}</h1>
+                                    <p className="flex justify-center secondary-text mb-4">{agent.designation}</p>
+                                    <div className="flex justify-center gap-5">
+                                        <button>
+                                            <MdMail className="h-10 w-10 bg-slate-100 rounded-full text-gray-900 p-2" />
+                                        </button>
+                                        <button>
+                                            <FaPhoneAlt className="h-10 w-10 bg-slate-100 rounded-full text-gray-900 p-2" />
+                                        </button>
+                                    </div>
+                                    <button className="absolute top-5 right-5">
+                                        <FaPlus className="h-10 w-10 bg-gray-900 rounded-full text-white p-2" />
+                                    </button>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
             </div>
