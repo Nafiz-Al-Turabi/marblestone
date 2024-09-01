08116e9698d6bb5815d052c464e802cbd1535ad0
unknown <nafizalturabi18cse@gmail.com>
2024-09-01 11:23:11 +0600

    Contact message fetch in admin dashboard

--- src/Admin Components/Messages/Messages.jsx
+++ src/Admin Components/Messages/Messages.jsx
@@ -1,22 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaStar } from 'react-icons/fa';
 import FullMessage from '../FullMessage/FullMessage';
 import { IoMdTrash } from 'react-icons/io';
+import axiosInstance from '../../Axios/AxiosInstance';
 
 const Messages = () => {
     const [selectedRows, setSelectedRows] = useState([]);
     const [openFullMessage, setOpenFullMessage] = useState(false);
+    const [messages, setMessages] = useState([]);
 
     const toggleOpenMessage = () => {
         setOpenFullMessage(!openFullMessage)
     }
 
-    const messages = [
-        { id: 1, name: 'Nafiz', message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', date: '20 Mar' },
-        { id: 2, name: 'Mofiz', message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', date: '21 Mar' },
-        { id: 3, name: 'Masud', message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', date: '5 Mar' },
-        // Add more messages here
-    ];
+    // const messages = [
+    //     { id: 1, name: 'Nafiz', message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', date: '20 Mar' },
+    //     { id: 2, name: 'Mofiz', message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', date: '21 Mar' },
+    //     { id: 3, name: 'Masud', message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', date: '5 Mar' },
+    //     // Add more messages here
+    // ];
+    
+    useEffect(()=>{
+        fetchMessage()
+    },[])
+
+    const fetchMessage = async () => {
+        try {
+            const response = await axiosInstance.get('/contacts');
+            if (response) {
+                setMessages(response.data);
+                console.log(response.data)
+            }
+        } catch (error) {
+
+        }
+    }
 
     const handleCheckboxChange = (id) => {
         setSelectedRows(prevSelectedRows =>
@@ -41,17 +59,17 @@ const Messages = () => {
                     <table className='min-w-full bg-white shadow rounded-lg '>
                         <tbody>
                             {messages.map(message => (
-                                <tr key={message.id} className='hover:bg-gray-50 duration-300 cursor-pointer'>
+                                <tr key={message._id} className='hover:bg-gray-50 duration-300 cursor-pointer'>
                                     <td className='py-[15px] px-4 flex items-center space-x-5'>
                                         <input
                                             type='checkbox'
-                                            checked={selectedRows.includes(message.id)}
-                                            onChange={() => handleCheckboxChange(message.id)}
+                                            checked={selectedRows.includes(message._id)}
+                                            onChange={() => handleCheckboxChange(message._id)}
                                             className='form-checkbox h-5 w-5 text-green-500'
                                         />
                                         <FaStar className='text-yellow-500' />
                                     </td>
-                                    <td className='py-2 px-4 text-gray-800'>{message.name}</td>
+                                    <td className='py-2 px-4 text-gray-800'>{message.firstName + message.lastName}</td>
                                     <td onClick={toggleOpenMessage} className='py-2 px-4 text-gray-800'>{message.message}</td>
                                     <td className='py-2 px-4 text-gray-400'>{message.date}</td>
                                 </tr>
